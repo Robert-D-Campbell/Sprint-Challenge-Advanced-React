a2c7c4a99d4915cef88ba72b2789471c7747455e
Robert Campbell <robertc1191@Roberts-MacBook-Pro-2.local>
2019-11-01 10:11:29 -0700

    mvp done

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {render, getByTestId} from '@testing-library/react';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from './App';
+import PlayerCard from './components/PlayerCard'
+import PlayerList from './components/PlayerList'
+
+test('renders without crashing', () => {
+  render(<App />);
+
 });
+
+test('render Player List Component without crashing', () => {
+  render(<PlayerList />);
+})
+
+test('it returns the header text', () => {
+  const {getByText} = render(<App />)
+  getByText(/women's world cup/i)
+})
+
+test('it returns Text content of id="player-name"', () => {
+  const {getByTestId} = render(<PlayerCard />)
+  expect(getByTestId('player-name').textContent).toBe('')
+})
